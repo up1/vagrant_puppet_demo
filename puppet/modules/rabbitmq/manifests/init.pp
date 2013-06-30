5ba838e4879af0022466feb32cb3ac916bd6a3a7
boyone <boyone.g@gmail.com>
2013-06-30 13:47:43 +0700

    add exec:add_rabbitmq_vhost, add_rabbitmq_user and add_rabbitmq_permission

--- puppet/modules/rabbitmq/manifests/init.pp
+++ puppet/modules/rabbitmq/manifests/init.pp
@@ -11,4 +11,30 @@ class rabbitmq {
         hasrestart => true,
         require => Package["rabbitmq-server"],
     }
+
+    exec { 'add-geeky-vhost':
+        command => "rabbitmqctl add_vhost /geeky",
+        unless => 'rabbitmqctl list_vhosts | grep geeky',
+        path    => "/usr/bin:/usr/sbin:/bin:/sbin",
+        user => 'root',
+        group => 'root'   
+    }
+
+    exec { 'add-geeky-user':
+        command => 'rabbitmqctl add_user geeky geeky',
+        unless =>   "/usr/sbin/rabbitmqctl list_users | grep geeky",
+        path    => "/usr/bin:/usr/sbin:/bin:/sbin",
+        user => 'root',
+        group => 'root'
+    }
+
+  exec { 'set-geeky-permissions':
+        command => 'rabbitmqctl set_permissions -p /geeky geeky ".*" ".*" ".*"',
+        unless =>   "/usr/sbin/rabbitmqctl list_user_permissions geeky | grep '\\.\\*\\s\\.\\*\\s\\.\\*'",
+        path    => "/usr/bin:/usr/sbin:/bin:/sbin",
+        user => 'root',
+        group => 'root'
+  }
+
+  Service['rabbitmq-server'] -> Exec['add-geeky-vhost'] -> Exec['add-geeky-user'] -> Exec['set-geeky-permissions']
 }
